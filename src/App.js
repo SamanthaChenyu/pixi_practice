d0dca8dac29a67fe16feea7bef4a876d198b49a7
SamanthaChenyu <same0730@gmail.com>
2021-08-12 11:34:53 +0800

    [Animate]

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ import body from './body.png';
 import backFooterRight from './backFooter_Right.png';
 import forntFooterRight from './frontFooter_Right.png';
 import frontFooterLeft from './frontFooter_Left.png';
+import unicon from './unicon.png';
+import tail from './tail.png';
 
 const { Texture } = PIXI;
 
@@ -24,25 +26,25 @@ const BunniesContainer = ({ ...props }) => {
 
   useTick((delta) => {
     
-  function test(i) {
+  function walk(i) {
     const positionY = Math.sin(i/2) * 10;
     if(positionY > 9) {
-      //console.log(positionY)
       return 9.99;
     }
       return positionY;
   }
 
     setI(i => i + 0.08 * delta);
-    setY(test(i));
+    setY(walk(i));
   });
 
   return (
     <Container x={250} y={250} {...props}>
       
-      <Sprite x={-78} y={y + 90} anchor={0.5} scale={0.5} rotation={Math.cos(i) * -0.4} image={frontFooterLeft} />      
+      <Sprite x={-78} y={y + 90} anchor={0.5} scale={0.5} rotation={Math.cos(i) * -0.4} image={frontFooterLeft} /> 
+      <Sprite x={135} y={y + 55} anchor={0.5} scale={0.5} image={tail} />      
       <Sprite x={0} y={y} anchor={0.5} image={body} rotation={0} scale={0.5}/>
-
+      <Sprite x={-50} y={y +-90} anchor={0.5} image={unicon} scale={0.5} />
       <Sprite x={80} y={y + 90} anchor={0.5} scale={0.5} rotation={Math.cos(i) * -0.35} image={backFooterRight} />
      {/* frontFooter(left) */}
       <Sprite x={10} y={y + 100} anchor={0.5} scale={0.5} rotation={Math.cos(i) * 0.35} image={forntFooterRight} />
